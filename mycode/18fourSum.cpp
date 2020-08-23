650f0ad6b4805daca3bc742db006a2b1aba5b29e
NoahMax1997 <1076444637@qq.com>
2020-08-23 14:34:19 +0800

    "test"

--- mycode/18fourSum.cpp
+++ mycode/18fourSum.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+    }
+};
 int main (){
     cout<<endl;
     system("pause");
