5e2d90c3df4002b8bc46109703979d076c4b0af1
NoahMax1997 <1076444637@qq.com>
2020-08-23 19:43:38 +0800

    optimize

--- mycode/18fourSum.cpp
+++ mycode/18fourSum.cpp
@@ -1,13 +1,54 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+//复杂度极高
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
+        vector<vector<int>> ret;
+        vector<int> t_vec;
+        if(nums.size() < 4) return ret;
+        sort(nums.begin(),nums.end());
+        for (int i = 0; i < nums.size() ; i++)
+        {
+            /* code */
+            if(i>0&&nums[i-1]==nums[i]){
+                continue;
+            }
+            t_vec.push_back(nums[i]);
+            for(int j=i+1;j<nums.size();j++){
+                if(j>i+1&&nums[j-1]==nums[j]){
+                    continue;
+                }
+                t_vec.push_back(nums[j]);
+                int left=j+1,right=nums.size()-1;
+                while(left<right){
+                    if(nums[i]+nums[j]+nums[left]+nums[right]==target){
+                        t_vec.push_back(nums[left]);
+                        t_vec.push_back(nums[right]);
+                        ret.push_back(t_vec);
+                        t_vec.pop_back();
+                        t_vec.pop_back();
+                        left++;
+                        while(left<right&&nums[left-1]==nums[left]){
+                            left++;
+                        }
+                    }else if(nums[i]+nums[j]+nums[left]+nums[right]>target){
+                        right--;
+                    }else{
+                        left++;
+                    }
+                }
+                t_vec.pop_back();
+            }
+            t_vec.pop_back();
+        }
+        return ret;
     }
 };
 int main (){
-    cout<<endl;
+    
     system("pause");
     return 0;
 }
