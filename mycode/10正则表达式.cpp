9d99a0e930c1c257a563dcd84993d2804ddcdb22
Noah <31157616+NoahMax1997@users.noreply.github.com>
2020-08-26 19:48:49 +0800

    regular_expression

--- mycode/10正则表达式.cpp
+++ mycode/10正则表达式.cpp
@@ -2,13 +2,69 @@
 #include <vector>
 using namespace std;
 class Solution {
+private:
+    inline int isSpecial(char ch){
+        if(ch=='*') return 1;
+        else if(ch=='.') return 2;
+        else return 0;
+    }
 public:
     bool isMatch(string s, string p) {
-        
+        int first_index=0,second_index=0;
+        while(first_index<s.size()&&second_index<p.size()){
+            char t_ch=p[second_index];
+            if(isSpecial(t_ch)==0){
+                if(s[first_index]==t_ch){
+                    first_index++;
+                    second_index++;
+                }else{
+                    if(second_index+1==p.size()||p[second_index+1]!='*'){
+                        return false;
+                    }else{
+                        // first_index++;
+                        second_index+=2;
+                    }
+                }
+            }else if(isSpecial(t_ch)==1){
+                if(p[second_index-1]==s[first_index]){
+                    while(first_index<s.size()&&p[second_index-1]==s[first_index])
+                        first_index++;
+                    second_index++;
+                    while(second_index<p.size()&&p[second_index-2]==p[second_index])
+                        second_index++;
+                }else{
+                    // first_index++;
+                    second_index++;
+                }
+            }else{
+                if(second_index<p.size()&&p[second_index+1]=='*'){
+                    while(second_index<p.size()&&isSpecial(p[second_index])!=0){
+                        second_index++;
+                    }
+                    if(second_index>=p.size()){
+                        return true;
+                    }else{
+                        int i;
+                        for(i=s.size()-1;i>=0&&s[i]!=p[second_index];i--);
+                        first_index= i>=0?i:0;
+                    }
+                }else{
+                    p[second_index]=s[first_index];
+                    second_index++;
+                    first_index++;
+                }
+            }
+        }
+        if(first_index==s.size()&&second_index==p.size()){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
 int main (){
-	Solution a;
-	
-	return 0;
-} 
+    Solution a;
+    a.isMatch("aaa","ab*a");
+    system("pause");
+    return 0;
+}
--- mycode/6 Z型变换.cpp
+++ mycode/6 Z型变换.cpp
@@ -4,13 +4,14 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
-        for(i=0;i<numRows;i++){
+        // for(i=0;i<numRows;i++){
         	
-		} 
+		// } 
     }
 };
 int main (){
 	Solution a;
-	
+	cout<<endl;
+    system("pause");
 	return 0;
 } 
