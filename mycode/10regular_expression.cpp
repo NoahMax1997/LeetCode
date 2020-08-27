d1d6a60d5b020de6017224f74de042dca6eb6f02
Noah <31157616+NoahMax1997@users.noreply.github.com>
2020-08-27 16:30:28 +0800

    regular_dfs

--- mycode/10regular_expression.cpp
+++ mycode/10regular_expression.cpp
@@ -8,7 +8,11 @@ public:
     bool isMatch(string s, string p,int s_size,int p_size){
         if(s_size==0&&p_size==0){
             return true;
-        }else if(s_size==0&&p_size!=0||s_size!=0&&p_size==0){
+        }else if(s_size!=0&&p_size==0){
+            return false;
+        }else if(s_size==0&&p_size>=2&&p[p_size-1]=='*'){
+            return isMatch(s,p,s_size,p_size-2);
+        }else if(s_size==0){
             return false;
         }
         if(s[s_size-1]==p[p_size-1]||p[p_size-1]=='.'){
@@ -19,24 +23,14 @@ public:
             while(s_size>=0){
                 p_size-=2;
                 if(isMatch(s,p,s_size,p_size)){
-                    ret=true;
+                    return true;
                 }
                 p_size+=2;
                 s_size--;
             }
             return ret;
         }else if(p[p_size-1]=='*'&&s[s_size-1]==p[p_size-2]){
-            bool ret=false;
-            // s_size--;
-            while(s_size>=0&&s[s_size-1]==p[p_size-2]){
-                p_size-=2;
-                if(isMatch(s,p,s_size,p_size)){
-                    ret=true;
-                }
-                p_size+=2;
-                s_size--;
-            }
-            return ret;
+            
         }else if(p[p_size-1]=='*'&&s[s_size-1]!=p[p_size-2]){
             return isMatch(s,p,s_size,p_size-2);
         }else{
@@ -53,12 +47,12 @@ int main()
 {
     Solution a;
     // cout<<a.isMatch("aa", "a")<<endl;
-    // system("pause");
-    cout<<a.isMatch("aa", "a*")<<endl;
-    // system("pause");
-    cout<<a.isMatch("ab", ".*")<<endl;
-    cout<<a.isMatch("aab", "c*a*b")<<endl;
-    cout<<a.isMatch("mississippi", "mis*is*p*.")<<endl;
+    // cout<<a.isMatch("aa", "a*")<<endl;
+    // cout<<a.isMatch("ab", ".*")<<endl;
+    // cout<<a.isMatch("aab", "c*a*b")<<endl;
+    // cout<<a.isMatch("mississippi", "mis*is*p*.")<<endl;
+    // cout<<a.isMatch("mississippi", "mis*is*ip*.")<<endl;
+    cout<<a.isMatch("a", ".*..a*")<<endl;
     system("pause");
     return 0;
 }
