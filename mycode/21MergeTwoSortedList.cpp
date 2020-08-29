4bb2b8d9c731e292a17eaf3446d48dbb1594ae34
NoahMax1997 <1076444637@qq.com>
2020-08-30 00:05:14 +0800

    list init

--- mycode/21MergeTwoSortedList.cpp
+++ mycode/21MergeTwoSortedList.cpp
@@ -24,28 +24,39 @@ class Solution
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
     {
-        ListNode *ret=NULL,*l1_nonius=l1,*l2_nonius=l2,*tail=ret;
+        if(l1==NULL&&l2==NULL) return NULL;
+        ListNode *ret=new ListNode(),*l1_nonius=l1,*l2_nonius=l2,*tail=ret;
         while(l1_nonius!=NULL&&l2_nonius!=NULL){
-            cout<<l1_nonius->val<<" "<<l2_nonius->val<<endl;
+            // cout<<l1_nonius->val<<" "<<l2_nonius->val<<endl;
             if(l1_nonius->val > l2_nonius->val){
-                tail=new ListNode(l2_nonius->val,NULL);
-                tail=tail->next;
+                *tail=*l2_nonius;
+                // tail->val=l2_nonius->val;
                 l2_nonius=l2_nonius->next;
             }else{
-                tail=new ListNode(l1_nonius->val,NULL);
-                tail=tail->next;
+                *tail=*l1_nonius;
+                // tail->val=l1_nonius->val;
                 l1_nonius=l1_nonius->next;
             }
+            tail->next=new ListNode();
+            tail=tail->next;
         }
         while(l1_nonius!=NULL){
-            tail=new ListNode(l1_nonius->val,NULL);
-            tail=tail->next;
+            *tail=*l1_nonius;
+            // tail->val=l1_nonius->val;
             l1_nonius=l1_nonius->next;
+            if(l1_nonius!=NULL){
+                tail->next=new ListNode();
+            }
+            tail=tail->next;
         }
         while(l2_nonius!=NULL){
-            tail=new ListNode(l2_nonius->val,NULL);
-            tail=tail->next;
+            *tail=*l2_nonius;
+            // tail->val=l2_nonius->val;
             l2_nonius=l2_nonius->next;
+            if(l2_nonius!=NULL){
+                tail->next=new ListNode();
+            }
+            tail=tail->next;
         }
         return ret;
     }
@@ -54,27 +65,25 @@ int main()
 {
     Solution a;
     ListNode *l1,*l2;
-    l1=new ListNode(1,NULL);
-    ListNode* h1=l1;
-    l1=l1->next;
-    l1=new ListNode(2,NULL);
-    l1=l1->next;
-    l1=new ListNode(4,NULL);
-    l1=l1->next;
-    l2=new ListNode(1,NULL);
-    ListNode* h2=l2;
-    l2=l2->next;
-    l2=new ListNode(3,NULL);
-    l2=l2->next;
-    l2=new ListNode(4,NULL);
-    l2=l2->next;
-    // ListNode *t= a.mergeTwoLists(h1,h2);
-    ListNode *t= h1;
-    while(t){
-        cout<<t->val<<endl;
-        t=t->next;
-    }
-    t= h2;
+    l1=new ListNode(1,new ListNode(2,new ListNode(4,nullptr)));
+    l2=new ListNode(1,new ListNode(3,new ListNode(4,nullptr)));
+    ListNode *h1=l1,*h2=l2;
+    // l1->val=1;
+    // l1=l1->next;//l1只是获取了上一个节点的next的值；l1和上个节点的next是两个变量；给l1 new出新空间不会让上个节点的next也有那个空间
+    // l1=new ListNode();
+    // l1->val=2;
+    // l1=l1->next;
+    // l1=new ListNode();
+    // l1->val=4;
+    // l1=l1->next;
+    // l1=NULL;
+    
+    // l2=new ListNode());
+    // ListNode* h2=l2;
+
+
+    ListNode *t;
+    t= a.mergeTwoLists(h1,h2);
     while(t){
         cout<<t->val<<endl;
         t=t->next;
