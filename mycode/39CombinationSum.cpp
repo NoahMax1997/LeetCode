63eaca7bf476abcfc06b1309f3fb3cfd405f30cc
NoahMax1997 <1076444637@qq.com>
2020-09-08 01:28:41 +0800

    39

--- mycode/39CombinationSum.cpp
+++ mycode/39CombinationSum.cpp
@@ -5,27 +5,44 @@
 using namespace std;
 class Solution {
 public:
+    void dfs(const int& target,const vector<int>& candidates,vector<vector<int>>& ret,vector<int>& t_vec,int &sum,int &index){
+        if(sum==target){
+            // for(auto i:t_vec){
+            //     cout<<i<<" ";
+            // }
+            // cout<<endl;
+            ret.push_back(t_vec);
+            return;
+        }
+        for(int i=index;i<candidates.size();i++){
+            if(sum+candidates[i]>target){
+                // break;//排好序
+                continue;//未排序
+            }else{
+                sum+=candidates[i];
+                t_vec.push_back(candidates[i]);
+                dfs(target,candidates,ret,t_vec,sum,i);
+                sum-=candidates[i];
+                t_vec.pop_back();
+            }
+        }
+    }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int i=0,sum=0;
         vector<vector<int>> ret;
         vector<int> t_vec;
-        stack<int> st;
-        for(int i=0;i<candidates.size();i++){
-            st.push(candidates[i]);    
-        }
-        while(!st.empty()){
-            
-        }
+        int sum=0;
+        dfs(target,candidates,ret,t_vec,sum);
+        return ret;
     }
 };
 int main()
 {
     Solution a;
-    vector<int> t={4,2,0,2,3,2,0};
-    
-    for(auto i:t){
-        cout<<i<<endl;
-    }
+    vector<int> t={2,3,6,7};
+    // for(auto t:a.combinationSum(t,7))
+    // for(auto i:t){
+    //     cout<<i<<endl;
+    // }
     system("pause");
     return 0;
 }
